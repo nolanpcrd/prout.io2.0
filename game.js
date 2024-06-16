d00bcc9d1be658195908217f070a4428ce9f8a7b
nolanpcrd <81513481+nolanpcrd@users.noreply.github.com>
2024-06-16 02:42:32 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,6 @@
 let name;
 let x=0;
 let y=0;
-let map = document.getElementById("map");
-
-let playerMiniMap = document.getElementById("mini-map-player");
-let xPlayerMiniMap = 10;
-let yPlayerMiniMap = 5;
-playerMiniMap.style.left = xPlayerMiniMap + "%";
-playerMiniMap.style.top = yPlayerMiniMap + "%";
 
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -40,10 +33,6 @@ function gameLoop() {
         x -= 10;
         xPlayerMiniMap += 0.082;
     }
-    map.style.top = y + "px";
-    map.style.left = x + "px";
-    playerMiniMap.style.top = yPlayerMiniMap + "%";
-    playerMiniMap.style.left = xPlayerMiniMap + "%";
     requestAnimationFrame(gameLoop);
 }
 
